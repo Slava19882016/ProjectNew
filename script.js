a9f759b5b4dcf15bcb76fe4cd2baa0bb0fdd9f14
Slava <vp1891719@gmail.com>
2021-02-03 21:09:52 +0300

    if else

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const numberOfFilms = prompt ('Сколько фильмов Вы посмотрели?', '');
+'use strict'
+
+const numberOfFilms = prompt('Сколько фильмов Вы посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -9,13 +11,31 @@ const personalMovieDB = {
 };
 
 
-const a = prompt ("Последний просмотренный фильм", ""),
-      b = prompt ("Ваша оценка фильму", ""),
-      c=  prompt ("Последний просмотренный фильм", ""),
-      d = prompt ("Ваша оценка фильму", "");
 
 
-personalMovieDB.movie[a] = b,
-personalMovieDB.movie[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Последний просмотренный фильм", ""),
+        b = prompt("Ваша оценка фильму", "");
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movie[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+
+
+};
+
+if (personalMovieDB.count < 10) {
+    console.log ('Мало посмотрели');
+} else if (personalMovieDB.count <= 10 && personalMovieDB.count <30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log ('Ошибка');
+}
 
 console.log(personalMovieDB);
